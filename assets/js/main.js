6bd3f0b6286c6b00b699496703c80ba7d48d88f9
Marcoseven <marco.caliciotti3@gmail.com>
2021-10-25 15:26:38 +0200

    Aggiunte modifiche
    Inseriti due array di oggetti, un ciclo for, le variabili e i vari commenti.

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,3 +14,45 @@ Bonus 2: Gestire l'assenza dell'immagine profilo con un elemento di fallback che
 Bonus 3: Click al pulsante "Mi Piace" incrementa il counter dei like al post.
 */
 
+// DOM
+let card = document.querySelector(".card");
+
+// Array di oggetti
+const posts = [
+	{
+		id: 1,
+		contenuto: "immagine e testo",
+		immagine: "https://unsplash.it/300/300?image=",
+		autore: "Alberto Bisetti",
+		likes: 80,
+		dataCreazione: "4 mesi fa",
+	},
+	{
+		id: 2,
+		contenuto: "immagine e testo",
+		immagine: "https://unsplash.it/300/300?image=",
+		autore: "Alessandro Fedele",
+		likes: 50,
+		dataCreazione: "2 mesi fa",
+	},
+	{
+		id: 3,
+		contenuto: "immagine e testo",
+		immagine: "https://unsplash.it/300/300?image=",
+		autore: "Camilla Necci",
+		likes: 120,
+		dataCreazione: "1 mese fa",
+	},
+];
+// console.log(posts);
+
+// Ciclare nell'array di oggetti
+for (let index = 0; index < posts.length; index++) {
+	const element = posts[index];
+
+	console.log(element);
+}
+
+// 2° array di oggetti composto dagli id del 1° array
+const { id } = posts[0, 1, 2];
+console.log(id);
