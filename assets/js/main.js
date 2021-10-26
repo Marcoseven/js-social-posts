21d6b85120578549b86bfdc46e6ad842c0347364
Marcoseven <marco.caliciotti3@gmail.com>
2021-10-26 09:25:22 +0200

    Aggiunte modifiche
    Modificato alcune variabili e inseriti i commenti.

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,44 +15,71 @@ Bonus 3: Click al pulsante "Mi Piace" incrementa il counter dei like al post.
 */
 
 // DOM
-let card = document.querySelector(".card");
+let row = document.querySelector(".row");
 
 // Array di oggetti
 const posts = [
 	{
 		id: 1,
-		contenuto: "immagine e testo",
-		immagine: "https://unsplash.it/300/300?image=",
-		autore: "Alberto Bisetti",
+		content:
+			"Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Etnihil ullam aut alias.",
+		image: "https://unsplash.it/300/300?image=",
+		author: ["Alberto Bisetti", "https://unsplash.it/300/300?image="],
 		likes: 80,
-		dataCreazione: "4 mesi fa",
+		date: "4 mesi fa",
 	},
 	{
 		id: 2,
-		contenuto: "immagine e testo",
-		immagine: "https://unsplash.it/300/300?image=",
-		autore: "Alessandro Fedele",
+		content:
+			"Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Etnihil ullam aut alias.",
+		image: "https://unsplash.it/300/300?image=",
+		author: ["Alessandro Fedele", "https://unsplash.it/300/300?image="],
 		likes: 50,
-		dataCreazione: "2 mesi fa",
+		date: "2 mesi fa",
 	},
 	{
 		id: 3,
-		contenuto: "immagine e testo",
-		immagine: "https://unsplash.it/300/300?image=",
-		autore: "Camilla Necci",
+		content:
+			"Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Etnihil ullam aut alias.",
+		image: "https://unsplash.it/300/300?image=",
+		author: ["Camilla Necci", "https://unsplash.it/300/300?image="],
 		likes: 120,
-		dataCreazione: "1 mese fa",
+		date: "1 mese fa",
 	},
 ];
 // console.log(posts);
 
-// Ciclare nell'array di oggetti
-for (let index = 0; index < posts.length; index++) {
-	const element = posts[index];
+// Ciclo for nell'array di oggetti
 
-	console.log(element);
+post(posts);
+function post(posts) {
+	for (let index = 0; index < posts.length; index++) {
+		const element = posts[index];
+		let elementPosts = `
+                 <div class="card">
+					<div class="header d_flex">
+						<img src="${posts[index].author[1]}" alt="" />
+						<div class="text">
+							<h2>${posts[index].author[0]}</h2>
+							<h3>${posts[index].date}</h3>
+						</div>
+					</div>
+					<div class="main">
+                        <p>${posts[index].content}</p>
+						<img src="${posts[index].image}" alt="" />
+					</div>
+					<div class="social">
+						<div><button><i class="fas fa-thumbs-up"></i> Mi Piace</button></div>
+						<div>Piace a ${posts[index].likes} persone</div>
+					</div>
+				</div>
+    `;
+		//row.insertAdjacentHTML("beforeend", elementPosts);
+		row.innerHTML += elementPosts;
+	}
 }
 
 // 2° array di oggetti composto dagli id del 1° array
-const { id } = posts[0, 1, 2];
+const { id } = posts[(0, 1, 2)];
 console.log(id);
+ 
